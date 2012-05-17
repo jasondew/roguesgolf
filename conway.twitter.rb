1ce9a79dc3a39e85f0c4b839cfcb2e073b939619
Jason Dew <jason.dew@gmail.com>
2012-05-16 20:51:53 -0400

    renaming

--- conway.twitter.rb
+++ conway.twitter.rb
@@ -1,3 +1,4 @@
-require"zlib";eval Zlib::Inflate.inflate %{
-
-}
+n=(0..3);g=[!1]*4;b=[g,g,[!1]+[1]*3,g]
+4.times{b=n.map{|x|n.map{|y|l=[x-1,x+1,x].product([y-1,y+1,y])[0..7].select{|f,g|(r=b[f])&&r[g]}.size
+l==3||b[x][y]&&l==2}}
+b.map{|r|puts r.map{|c|c ??#:?.}*""}}
